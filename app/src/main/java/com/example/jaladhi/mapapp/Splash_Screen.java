9abcdfbaa9e49ff8cbb5ead25dbca6bc2cd9d216
jaladhipathak <jaladhipathak14@gmail.com>
2018-02-11 22:58:46 +0530

    add splash screen

--- app/src/main/java/com/example/jaladhi/mapapp/Splash_Screen.java
+++ app/src/main/java/com/example/jaladhi/mapapp/Splash_Screen.java
@@ -6,7 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Splash_Screen extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT = 5000;
+    private static int SPLASH_TIME_OUT = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
