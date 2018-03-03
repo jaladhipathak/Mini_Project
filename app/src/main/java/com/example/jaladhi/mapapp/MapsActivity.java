6750b3c383def72c90c204d20235bf50712f274f
jaladhipathak <jaladhipathak14@gmail.com>
2018-03-03 17:06:03 +0530

    Combined nav drawer with map activity

--- app/src/main/java/com/example/jaladhi/mapapp/MapsActivity.java
+++ app/src/main/java/com/example/jaladhi/mapapp/MapsActivity.java
@@ -4,12 +4,24 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.Configuration;
 import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.location.Location;
 import android.location.LocationListener;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -19,22 +31,60 @@ import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     double CurrentLatitude,CurrentLongitude;
     private BroadcastReceiver broadcastReceiver;
     Intent intent2;
+    Toolbar toolbar;
+    private DrawerLayout myDrawerlayout;
+    private android.support.v7.app.ActionBarDrawerToggle drawerToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.content_side_nav);
+        toolbar = (Toolbar) findViewById(R.id.toolbar1);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.menubutton);
+        myDrawerlayout =findViewById(R.id.drawer_layout);
+        NavigationView navigationView=findViewById(R.id.nav_view);
+        drawerToggle= setupDrawerToggle();
+        myDrawerlayout.addDrawerListener(drawerToggle);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                        item.setChecked(true);
+                        myDrawerlayout.closeDrawers();
+                        return true;
+
+                    }
+                }
+        );
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         intent2 = new Intent(MapsActivity.this, GoogleService.class);
         startService(intent2);
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(broadcastReceiver, new IntentFilter(GoogleService.str_receiver));
+        startService(intent2);
+
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
         broadcastReceiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
@@ -50,15 +100,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         };;
 
     }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        registerReceiver(broadcastReceiver, new IntentFilter(GoogleService.str_receiver));
-        startService(intent2);
-
-    }
-
     @Override
     protected void onPause() {
         super.onPause();
@@ -84,5 +125,81 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 //        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker"));
 //        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+    private android.support.v7.app.ActionBarDrawerToggle setupDrawerToggle(){
+        return new ActionBarDrawerToggle(this, myDrawerlayout, toolbar, R.string.navigation_drawer_open,  R.string.navigation_drawer_close);
+    }
+    @Override
+    public void onBackPressed() {
 
+        if (myDrawerlayout.isDrawerOpen(GravityCompat.START)) {
+            myDrawerlayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //getMenuInflater().inflate(R.menu.drawer_view, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        //int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+//        if (id == android.R.id.home) {
+//            myDrawerlayout.openDrawer(GravityCompat.START);
+//            return true;
+//        }
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.newpost) {
+            // Handle the camera action
+        } else if (id == R.id.smypost) {
+
+        } else if (id == R.id.sylove) {
+
+        } else if (id == R.id.chcity) {
+
+        } else if (id == R.id.refresh) {
+
+        } else if (id == R.id.logout) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggles
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
 }
+
