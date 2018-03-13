57484d788e6ac07de408993aad2b8bedcb2df964
jaladhipathak <jaladhipathak14@gmail.com>
2018-03-13 10:13:53 +0530

    Integrated Gallery

--- app/src/main/java/com/example/jaladhi/mapapp/New_Post.java
+++ app/src/main/java/com/example/jaladhi/mapapp/New_Post.java
@@ -1,10 +1,13 @@
 package com.example.jaladhi.mapapp;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.ImageButton;
@@ -13,12 +16,17 @@ import android.widget.Toast;
 
 public class New_Post extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int RESULT_LOAD_IMG = 2;
     ImageButton imageButton;
+    ImageButton gallerybutton;
+    String imgstring;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new__post);
-        imageButton=(ImageButton)findViewById(R.id.cameraicon);
+        imageButton = (ImageButton) findViewById(R.id.cameraicon);
+        gallerybutton = (ImageButton) findViewById(R.id.galleryicon);
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -28,23 +36,48 @@ public class New_Post extends AppCompatActivity {
                 }
             }
         });
+        gallerybutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loadimageintent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(loadimageintent, RESULT_LOAD_IMG);
+            }
+        });
 
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Intent nintent=new Intent(New_Post.this,Show_Image_Preview.class);
-            if(extras!=null){
-                nintent.putExtras(extras);
-                startActivity(nintent);
-            }
-            else{
-                Toast tost= Toast.makeText(getApplicationContext(),"Please Select a Picture",Toast.LENGTH_SHORT);
-                tost.setGravity(Gravity.BOTTOM|Gravity.CENTER,0,0);
-                tost.show();
-            }
+        super.onActivityResult(requestCode,resultCode,data);
+        try {
+            if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+                Bundle extras = data.getExtras();
+                Intent nintent = new Intent(New_Post.this, Show_Image_Preview.class);
+                if (extras != null) {
+                    nintent.putExtras(extras);
+                    nintent.putExtra("mode","c");
+                    startActivity(nintent);
+                }
 
+            } else if (requestCode == RESULT_LOAD_IMG && data != null) {
+                if (true) {
+                    Uri selectedimage = data.getData();
+                    String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                    Cursor cursor = getContentResolver().query(selectedimage, filePathColumn, null, null, null);
+                    cursor.moveToFirst();
+                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    imgstring = cursor.getString(columnIndex);
+                    cursor.close();
+                    Intent nintent = new Intent(New_Post.this, Show_Image_Preview.class);
+                    nintent.putExtra("mode","g");
+                    nintent.putExtra("path", imgstring);
+                    startActivity(nintent);
+                } else {
+                    Toast.makeText(this, "You haven't picked Image", Toast.LENGTH_LONG).show();
+                }
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show();
         }
     }
 }
--- app/src/main/java/com/example/jaladhi/mapapp/Show_Image_Preview.java
+++ app/src/main/java/com/example/jaladhi/mapapp/Show_Image_Preview.java
@@ -1,6 +1,7 @@
 package com.example.jaladhi.mapapp;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -12,9 +13,16 @@ public class Show_Image_Preview extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show__image__preview);
         imageView=(ImageView) findViewById(R.id.preview);
-        Bundle extras = getIntent().getExtras();
-        Bitmap imageBitmap = (Bitmap) extras.get("data");
-        imageView.setImageBitmap(imageBitmap);
+        String mode=getIntent().getStringExtra("mode");
+        if(mode.equals("c")){
+            Bundle extras = getIntent().getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+        else if(mode.equals("g")){
+            String path=getIntent().getStringExtra("path");
+            imageView.setImageBitmap(BitmapFactory.decodeFile(path));
+        }
 
     }
 }
